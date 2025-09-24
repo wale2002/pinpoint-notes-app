516168a0454d3c87a27776e5e97eac8d8b73b148
wale2002 <gemred2002@gmail.com>
2025-09-24 20:45:13 +0100

    final update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = "https://stickynote-rosy.vercel.app/api/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
